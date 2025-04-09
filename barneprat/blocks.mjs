323016bb8af04806e61e0f3c37e0707816712906
softelandmarius <softelandmarius@gmail.com>
2025-04-09 19:06:21 +0200

    Update blocks.mjs

--- barneprat/blocks.mjs
+++ barneprat/blocks.mjs
@@ -4,7 +4,7 @@ import Button from "./blocks/button.mjs";
 import Sound from "./blocks/sound.mjs";
 import CellAnimation from "./blocks/cellAnimation.mjs";
 import Text from "./blocks/text.mjs";
-import SoundQue from "./blocks/SoundQue.mjs";
+import SoundQue from "./blocks/soundQue.mjs";
 
 
 export { Image, Button, Sound, CellAnimation, Text,SoundQue };
